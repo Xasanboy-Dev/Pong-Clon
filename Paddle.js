a0f6963689422ff13753ad88ac38b518d0628c5f
Xasanboy <hasanbojabdurasulov309@gmail.com>
2022-11-16 21:23:53 +0500

    finish

---
+++ Paddle.js
@@ -0,0 +1,22 @@
+const SPEED = 0.2
+export default class Paddle {
+    constructor(paddleElem) {
+        this.paddleElem = paddleElem
+        this.reset()
+    }
+    get position() {
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"))
+    }
+    set position(value) {
+        this.paddleElem.style.setProperty("--position", value)
+    }
+    reset() {
+        this.position = 50
+    }
+    rect() {
+        return this.paddleElem.getBoundingClientRect()
+    }
+    update(delta, ballHeight) {
+        this.position += SPEED * (ballHeight - this.position)
+    }
+}
